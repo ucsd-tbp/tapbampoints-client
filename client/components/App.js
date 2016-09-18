4820ac06854c079308b7b8a23c505f6c5a0a25c8
Brian Tan <tanrui.brian@gmail.com>
2016-09-18 14:55:44 -0700

    Integrated a sidebar component

--- client/components/App.js
+++ client/components/App.js
@@ -1,9 +1,42 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+var Sidebar = require('react-sidebar').default;
+
 class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { sidebarTransitions: false };
+  }
+
+  componentWillMount() {
+    const mediaQueryList = window.matchMedia(`(min-width: 800px)`);
+    mediaQueryList.addListener(this.mediaQueryChanged.bind(this));
+
+    this.setState({
+      mediaQueryList: mediaQueryList,
+      sidebarDocked: mediaQueryList.matches
+    });
+  }
+
+  componentWillUnmount() {
+    this.state.mediaQueryList.removeListener(this.mediaQueryChanged.bind(this));
+  }
+
+  mediaQueryChanged() {
+    this.setState({ sidebarDocked: this.state.mediaQueryList.matches });
+  }
+
   render() {
-    return <h3>App component.</h3>
+    const sidebarContent = <h3>Sidebar content.</h3>;
+
+    return (
+      <Sidebar sidebar={sidebarContent}
+               docked={this.state.sidebarDocked}
+               transitions={this.state.sidebarTransitions}>
+        <h3>Main content.</h3>
+      </Sidebar>
+    );
   }
 }
 
