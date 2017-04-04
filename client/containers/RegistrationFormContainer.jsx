0c2c48e7e02f154f8f474a1bc08e64014e6975c6
Brian Tan <tanrui.brian@gmail.com>
2017-04-03 18:28:31 -0700

    Added error checking to registration form

--- client/containers/RegistrationFormContainer.jsx
+++ client/containers/RegistrationFormContainer.jsx
@@ -1,14 +1,40 @@
 import React from 'react';
+import { browserHistory } from 'react-router';
 
 import RegistrationForm from '../components/RegistrationForm';
 import { EMAIL_REGEX, Houses, MIN_PASSWORD_LENGTH, Roles } from '../modules/constants';
+import Auth from '../modules/Auth';
+import API from '../modules/API';
 import Events from '../modules/Events';
+
+function validate(credentials) {
+  const errors = [];
+
+  if (!credentials.firstName || !credentials.lastName) {
+    errors.push('First or last name can\'t be blank.');
+  }
+
+  if (!EMAIL_REGEX.test(credentials.email)) {
+    errors.push('Email has an invalid format.');
+  }
+
+  if (credentials.password < MIN_PASSWORD_LENGTH) {
+    errors.push(`Password has to be at least ${MIN_PASSWORD_LENGTH} characters.`);
+  } else if (credentials.password !== credentials.passwordConfirmation) {
+    errors.push('Passwords don\'t match.');
+  }
+
+  if (!Events.parsePID(credentials.pid)) {
+    errors.push('PID is invalid.');
+  }
+
+  return errors;
+}
 
 class RegistrationFormContainer extends React.Component {
   constructor(props) {
     super(props);
 
-    this.validate = this.validate.bind(this);
     this.handleChange = this.handleChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
 
@@ -28,30 +54,6 @@ class RegistrationFormContainer extends React.Component {
     };
   }
 
-  validate(credentials) {
-    const errors = [];
-
-    if (!credentials.firstName || !credentials.lastName) {
-      errors.push('First or last name can\'t be blank.');
-    }
-
-    if (!EMAIL_REGEX.test(credentials.email)) {
-      errors.push('Email has an invalid format.');
-    }
-
-    if (credentials.password < MIN_PASSWORD_LENGTH) {
-      errors.push(`Password has to be at least ${MIN_PASSWORD_LENGTH} characters.`);
-    } else if (credentials.password !== credentials.passwordConfirmation) {
-      errors.push('Passwords don\'t match.');
-    }
-
-    if (!Events.parsePID(credentials.pid)) {
-      errors.push('PID is invalid.');
-    }
-
-    return errors;
-  }
-
   handleChange(event) {
     const credentials = this.state.credentials;
     credentials[event.target.name] = event.target.value;
@@ -62,10 +64,16 @@ class RegistrationFormContainer extends React.Component {
   handleSubmit(event) {
     event.preventDefault();
 
-    const errors = this.validate(this.state.credentials);
-    this.setState({ errors });
+    this.setState({ errors: validate(this.state.credentials) });
+    if (validate(this.state.credentials).length > 0) return;
 
-    // this.props.onAuthChange(true);
+    API.registerUser(this.state.credentials, true)
+      .then(() => Auth.verifyToken())
+      .then((user) => {
+        this.props.onAuthChange(user);
+        browserHistory.push('/');
+      })
+      .catch(errors => this.setState({ errors: errors.map(error => error.msg) }));
   }
 
   render() {
