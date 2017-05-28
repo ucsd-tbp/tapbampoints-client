4c7495096d46e94eee60a3b7ff0e8ebbe47ac240
Brian Tan <tanrui.brian@gmail.com>
2017-05-27 17:40:16 -0700

    Added template for EventViewContainer

---
+++ client/containers/EventViewContainer.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+
+class EventViewContainer extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      event: {},
+    };
+  }
+
+  componentDidMount() {
+
+  }
+
+  render() {
+
+  }
+}
+
+export default EventViewContainer;
